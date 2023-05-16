b6130f376a0a0964e28fc7ddfd1b2731aea67192
Jacob Pfeifer <jacob@pfeifer.dev>
2023-05-15 21:01:22 -0400

    refactor request to better handle locally found invalid addresses

--- sample/actions/verify_addresses.py
+++ sample/actions/verify_addresses.py
@@ -23,36 +23,32 @@ def verify_addresses(addresses: List[Address]) -> List[Tuple[Address, Address]]:
         "license": settings.smarty_api_license,
     }
 
+    # Initialize list of verified addresses with invalid addresses
+    verified_addresses = [Address(street="",city="",zipcode="",valid=False)] * len(addresses)
+
+    # Convert addresses to smarty api address input to validate against api
+    # limits. We set match to invalid and the input_id to make it easier to
+    # match addresses later
+    input_addresses = [
+        USAddressInput.from_address(addresses[i], match="invalid", input_id=str(i))
+        for i in range(0,len(addresses))
+    ]
+
+    # Filter out invalid addresses
+    input_addresses = [address.dict() for address in input_addresses if address.is_valid()]
+
     # Split addresses into chunks of MAX_ENTRIES_PER_REQUEST
-    address_chunks = [addresses[i : i + MAX_ENTRIES_PER_REQUEST] for i in range(0, len(addresses), 100)]
+    address_chunks = [input_addresses[i : i + MAX_ENTRIES_PER_REQUEST] for i in range(0, len(input_addresses), 100)]
 
     # Verify each chunk of addresses with smarty api
-    verified_addresses = []
     for chunk in address_chunks:
-        # Convert addresses to smarty api address input to validate against api limits
-        # We also set match to invalid to simplify matching requests to results
-        # Finally we set the input_id so we can match up requests after
-        # filtering our input
-
-        input_addresses = [USAddressInput.from_address(chunk[i], match="invalid", input_id=str(i)) for i in range(0,len(chunk))]
-
-        # get input body from filtered addresses
-        body = [address.dict() for address in input_addresses if address.is_valid()]
-
-
         # verify addresses with smarty api
-        res = requests.post(f"{settings.smarty_api_base_route}/street-address", json=body, params=params)
-
-        # Initialize response to invalid addresses
-        chunk_response = [Address(street="",city="",zipcode="",valid=False)] * len(chunk)
+        res = requests.post(f"{settings.smarty_api_base_route}/street-address", json=chunk, params=params)
 
         # Convert smarty response to internal address model
         # and match response to correct index in chunk
         for address in res.json():
-            chunk_response[int(address["input_id"])] = USAddressOutput.parse_obj(address).to_address()
-
-        # add chunk addresses to final list
-        verified_addresses += chunk_response
+            verified_addresses[int(address["input_id"])] = USAddressOutput.parse_obj(address).to_address()
 
     # Map the requested addresses to the matching verified addresses
     return list(partition(2, interleave([addresses, verified_addresses])))
